cecd1595c5be6c442371b845ae02f7d68b605093
Diana Rita Nanyanzi <31903212+d-rita@users.noreply.github.com>
2020-09-23 15:12:55 +0300

    Add login script to login users

---
+++ login.js
@@ -0,0 +1,42 @@
+document.querySelector('#login-form').addEventListener('submit', login);
+
+
+// utility functions
+function accessStorage(value) {
+    try {
+        let storage = JSON.parse(localStorage.getItem('appStorage'));
+        return storage[value];
+    }
+    catch (error){
+        console.error(`${value} does not exist. || ${error}`)
+    }
+}
+
+function createErrorSection(text) {
+    let errorDiv = document.createElement('p');
+    errorDiv.innerHTML = text;
+    errorDiv.style.color= 'red';
+    return errorDiv
+}
+
+function login(e){
+    e.preventDefault();
+
+    // TODO: set cookie/token per session || destroy cookie on logout
+    let emailValue = document.querySelector('#email-login').value
+    let passwordValue = document.querySelector('#password-login').value
+    
+    if (emailValue == accessStorage('email') && passwordValue == accessStorage('password')){
+        window.location.replace('home.html');
+        displayAllPosts();
+    }
+    else {
+        let error = createErrorSection("Invalid credentials")
+        document.querySelector("#login-form").append(error)
+    }
+}
+
+
+
+
+
